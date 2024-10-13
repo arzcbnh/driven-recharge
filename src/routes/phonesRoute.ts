b9dd42ecf86300646e4a488f2be8439b1d16b31f
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 10:12:40 -0300

    Link phones controller to route

--- src/routes/phonesRoute.ts
+++ src/routes/phonesRoute.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { validateRequest } from "#middleware";
 import { PhoneSchema } from "#schemas";
+import { phonesController } from "#controllers";
 
 export const phonesRoute = Router();
-phonesRoute.post("/phones", validateRequest(PhoneSchema));
+phonesRoute.post("/phones", validateRequest(PhoneSchema), phonesController.postPhones);
