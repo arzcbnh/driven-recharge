a5cfb5179293aa64ac36845fa10386651dfa20f2
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 14:33:37 -0300

    Match phone interfaces to specifications

--- src/protocols/Phone.ts
+++ src/protocols/Phone.ts
@@ -6,14 +6,18 @@ export interface Phone {
     cpf: string;
 }
 
-export interface PhoneRegistryRequest {
+interface PhoneBase {
     name: string;
     description: string;
-    carrier: "15" | "21" | "31" | "41";
     number: string;
     cpf: string;
 }
 
-export interface PhoneQueryResult extends PhoneRegistryRequest {
+export interface PhoneRegistryRequest extends PhoneBase {
+    carrier: "15" | "21" | "31" | "41";
+}
+
+export interface PhoneQueryResult extends PhoneBase {
     id: number;
+    carrier_id: number;
 }
