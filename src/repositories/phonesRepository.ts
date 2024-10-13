1716e9b668582ea470625a96847c16f8c68dcb62
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 17:35:32 -0300

    Delete unused repository functions

--- src/repositories/phonesRepository.ts
+++ src/repositories/phonesRepository.ts
@@ -1,17 +1,5 @@
-import { Phone, PhoneDatabaseEntry } from "#protocols";
+import { PhoneDatabaseEntry } from "#protocols";
 import { database } from "./database.js";
-
-function selectPhoneByNumber(number: string) {
-    return database.query<Phone>(`SELECT * FROM phones WHERE number = $1`, [number]);
-}
-
-function selectPhoneById(id: number) {
-    return database.query<Phone>(`SELECT * FROM phones WHERE id = $1`, [id])
-}
-
-function selectPhonesByCpf(cpf: string) {
-    return database.query<Phone>(`SELECT * FROM phones WHERE cpf = $1`, [cpf]);
-}
 
 function selectPhones(column: "id" | "number" | "cpf", value: number | string) {
     return database.query<PhoneDatabaseEntry>(`SELECT * FROM phones WHERE $1 = $2;`, [column, value]);
@@ -28,9 +16,6 @@ function insertPhone({ name, description, carrier_id, number, cpf }: Omit<PhoneD
 }
 
 export const phonesRepository = {
-    selectPhoneByNumber,
-    selectPhonesByCpf,
-    selectPhoneById,
     selectPhones,
     insertPhone,
 };
