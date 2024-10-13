02037dd6b32eb8c694fe985719266a307fc56856
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 15:40:10 -0300

    Define type for query result

--- src/repositories/carrier.repository.ts
+++ src/repositories/carrier.repository.ts
@@ -1,7 +1,8 @@
+import { CarrierDbEntry } from "#protocols";
 import { database } from "./database.js";
 
 function selectCarrier(id: number) {
-    return database.query(`SELECT * FROM carriers WHERE id = $1;`, [id]);
+    return database.query<CarrierDbEntry>(`SELECT * FROM carriers WHERE id = $1;`, [id]);
 }
 
 export const CarrierRepository = {
