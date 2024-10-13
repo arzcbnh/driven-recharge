8491a7f52a84eb70273f09c9a1d034dbc2aa1e41
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 18:14:11 -0300

    Validate recharge schema

--- src/routes/rechargesRoute.ts
+++ src/routes/rechargesRoute.ts
@@ -1,5 +1,7 @@
 import { rechargesController } from "#controllers";
+import { validateRequest } from "#middleware";
+import { RechargeSchema } from "#schemas";
 import { Router } from "express";
 
 export const rechargesRoute = Router();
-rechargesRoute.post("/recharges", rechargesController.postRecharge);
+rechargesRoute.post("/recharges", validateRequest(RechargeSchema), rechargesController.postRecharge);
