b83227a2f99ce879bf3ce1aa9dc59759fbd94498
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 11:57:08 -0300

    Create recharges controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,3 @@
 export { phonesController } from "./phonesController.js";
 export { documentController } from "./documentController.js";
+export { rechargesController } from "./rechargesController.js";
---
+++ src/controllers/rechargesController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+import { rechargesService } from "#services";
+
+async function postRecharge(req: Request, res: Response) {
+    await rechargesService.createRecharge(req.body);
+    res.status(httpStatus.CREATED);
+}
+
+export const rechargesController = {
+    postRecharge,
+}
