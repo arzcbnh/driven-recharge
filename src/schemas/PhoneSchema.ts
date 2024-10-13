0d40fe9a0763c9146c0479d4c7648895a386b33d
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 17:14:04 -0300

    Set carrier field to take a number in phone schema

--- src/schemas/PhoneSchema.ts
+++ src/schemas/PhoneSchema.ts
@@ -1,14 +1,14 @@
 import joi from "joi";
-import { Phone } from "#protocols";
+import { PhoneRegistryRequest } from "#protocols";
 
-export const PhoneSchema = joi.object<Phone>({
+export const PhoneSchema = joi.object<PhoneRegistryRequest>({
     name: joi.string().required(),
     description: joi.string().required(),
     number: joi
         .string()
         .pattern(/\d{11}/)
         .required(),
-    carrier: joi.string().pattern(/\d{2}/).required(),
+    carrier: joi.number().integer().required(),
     cpf: joi
         .string()
         .pattern(/\d{11}/)
