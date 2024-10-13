74625005372ef836fcf078a7edd9ee70ff58fe35
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-12 23:55:07 -0300

    Add generics to phones repository

--- src/repositories/phonesRepository.ts
+++ src/repositories/phonesRepository.ts
@@ -2,11 +2,11 @@ import { Phone } from "#protocols";
 import { database } from "./database.js";
 
 function selectPhoneByNumber(number: string) {
-    return database.query(`SELECT * FROM phones WHERE number = $1`, [number]);
+    return database.query<Phone>(`SELECT * FROM phones WHERE number = $1`, [number]);
 }
 
 function selectPhonesByCpf(cpf: string) {
-    return database.query(`SELECT * FROM phones WHERE cpf = $1`, [cpf]);
+    return database.query<Phone>(`SELECT * FROM phones WHERE cpf = $1`, [cpf]);
 }
 
 function insertPhone({ name, description, carrier, number, cpf }: Phone) {
