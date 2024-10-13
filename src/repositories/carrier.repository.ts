1fd74b4f7e8564baca739ac7860c3247ba68bf3d
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 15:46:06 -0300

    Enable selecting carrier by different columns

--- src/repositories/carrier.repository.ts
+++ src/repositories/carrier.repository.ts
@@ -1,8 +1,8 @@
 import { CarrierDbEntry } from "#protocols";
 import { database } from "./database.js";
 
-function selectCarrier(id: number) {
-    return database.query<CarrierDbEntry>(`SELECT * FROM carriers WHERE id = $1;`, [id]);
+function selectCarrier(column: "id" | "code", value: number) {
+    return database.query<CarrierDbEntry>(`SELECT * FROM carriers WHERE $1 = $2;`, [column, value]);
 }
 
 export const CarrierRepository = {
