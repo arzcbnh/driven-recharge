718d247d2810bcac975d84cca7a24dfb0381b1bf
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-13 22:00:34 -0300

    Handle 'not found' error

--- src/middleware/handleError.middleware.ts
+++ src/middleware/handleError.middleware.ts
@@ -1,4 +1,4 @@
-import { ConflictError } from "#error";
+import { ConflictError, NotFoundError } from "#error";
 import { ErrorRequestHandler } from "express";
 import httpStatus from "http-status";
 
@@ -6,6 +6,8 @@ import httpStatus from "http-status";
 export const handleError: ErrorRequestHandler = (err, req, res, next) => {
     if (err instanceof ConflictError) {
         res.status(httpStatus.CONFLICT).send(err.message);
+    } else if (err instanceof NotFoundError) {
+        res.status(httpStatus.NOT_FOUND).send(err.message);
     } else {
         res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
         console.error(err);
