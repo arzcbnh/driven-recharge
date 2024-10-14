f146458957af03de71536557cc8fa2cbc8b023cd
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-14 16:53:29 -0300

    Fix summary route not importing controller

--- src/routes/summary.route.ts
+++ src/routes/summary.route.ts
@@ -1,4 +1,5 @@
+import { SummaryController } from "#controllers";
 import { Router } from "express";
 
 export const SummaryRoute = Router();
-SummaryRoute.get("/summary/:document");
+SummaryRoute.get("/summary/:document", SummaryController.getSummary);
